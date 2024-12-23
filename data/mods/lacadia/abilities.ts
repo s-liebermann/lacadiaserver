03756d9c551ba5c8e398c6ec663efb6f72807a4b
s-liebermann <spencerliebermann12@gmail.com>
2024-12-23 16:04:56 -0600

    fix leech (works!)

--- data/mods/lacadia/abilities.ts
+++ data/mods/lacadia/abilities.ts
@@ -4,11 +4,11 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 
         shortDesc: "The user heals 25% of the damage it deals with direct attacks",
         num: 2000,
-	},
-   onAfterMoveSecondarySelfPriority: -1,
+        onAfterMoveSecondarySelfPriority: -1,
 		onAfterMoveSecondarySelf(pokemon, target, move) {
 			if (move.totalDamage && !pokemon.forceSwitchFlag) {
 				this.heal(move.totalDamage / 4, pokemon);
 			}
 		},
+	},
 }
