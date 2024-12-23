cf172cf0415d4575e6228b9ae5573cffe0777313
s-liebermann <spencerliebermann12@gmail.com>
2024-12-23 11:38:49 -0600

    tortarma fix

--- data/mods/lacadia/formats-data.ts
+++ data/mods/lacadia/formats-data.ts
@@ -11,6 +11,27 @@ export const FormatsData: {[k: string]: ModdedSpeciesFormatsData} = {
 	enkidros: {
 		tier: "OU",
 		isModded: true,
+	},
+	rocadocio: {
+		tier: "OU",
+		isModded: true,
+	},
+	iguavadon: {
+		tier: "OU",
+		isModded: true,
+	},
+	tortarmasolar: {
+		tier: "OU",
+		isModded: true,
+	},
+	tortarmapolar: {
+		tier: "OU",
+		isModded: true,
+	},
+	tortality: {
+		tiera: "OU",
+		isModded: true
 	}
+
 	
 };
